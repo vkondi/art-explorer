023fc2afe5ae5ca6eaf462109151a089e04cb3df
Vishwajeet Kondi <vkondi@gmail.com>
2025-02-18 14:03:26 +0530

    fix: resolve build issue

--- src/components/common/withContentWrapper/withContentWrapper.tsx
+++ src/components/common/withContentWrapper/withContentWrapper.tsx
@@ -1,11 +1,17 @@
-import React, { ComponentType } from 'react';
+import { ComponentType } from 'react';
 
 const withContentWrapper = <P extends object>(WrappedComponent: ComponentType<P>) => {
-  return (props: P) => (
+  const ComponentWithWrapper = (props: P) => (
     <div style={{ margin: '0 100px' }}>
       <WrappedComponent {...props} />
     </div>
   );
+
+  ComponentWithWrapper.displayName = `WithContentWrapper(${
+    WrappedComponent.displayName || WrappedComponent.name || 'Component'
+  })`;
+
+  return ComponentWithWrapper;
 };
 
 export default withContentWrapper;
